f42963303f4268442a724215438b064241700dbd
William Spearman <wms19@my.fsu.edu>
2022-06-25 18:59:12 -0400

    Created a basic layout for Quiz Class and added the JPA  dependency.

---
+++ src/main/java/com/QualityQuizzes/Quiz/Quiz.java
@@ -0,0 +1,41 @@
+package com.QualityQuizzes.Quiz;
+
+import java.util.List;
+
+public class Quiz {
+    //  Inner class of QuizQuestions that will populate a Quiz.
+    private  class QuizQuestions {
+        // Members ////////////////////////////////////////////////////////////////////////////////////////////////////
+        private String questionName;
+       private int         questionNumber;
+       private String  questionType;
+
+       // Methods /////////////////////////////////////////////////////////////////////////////////////////////////////
+        QuizQuestions (String questionName, int questionNumber, String questionType) {
+            this.questionName      = questionName;
+            this.questionNumber = questionNumber;
+            this.questionType         = questionType;
+        }
+
+        // Setters and Getters ////////////////////////////////////////////////////////////////////////////////////////
+        public String getQuestionName      () {return  questionName;}
+        public int        getQuestionNumber () {return  questionNumber;}
+        public String getQuestionType         () {return  questionType;}
+    }
+
+    // Members ////////////////////////////////////////////////////////////////////////////////////////////////////////
+    private String                                 quizName;
+    private List<QuizQuestions> quizQuestions;
+    private int                                         quizQuestionAmount;
+
+    //Methods /////////////////////////////////////////////////////////////////////////////////////////////////////////
+    Quiz(
+            String quizName,
+            List<QuizQuestions> quizQuestions,
+            int quizQuestionAmount
+    ) {
+        this.quizName = quizName;
+        this.quizQuestions = quizQuestions;
+        this.quizQuestionAmount = quizQuestionAmount;
+    }
+}
