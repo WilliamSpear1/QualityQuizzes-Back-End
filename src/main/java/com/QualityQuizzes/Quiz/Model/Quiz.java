486226a83bffcf190bf2a34d627bba9798cdd536
William Spearman <wspearman@protonmail.com>
2022-10-14 14:41:32 -0400

    Made the outter the class the entity with columns and generated ID.
    Still have to figure out how the inner class is going to work.

--- src/main/java/com/QualityQuizzes/Quiz/Model/Quiz.java
+++ src/main/java/com/QualityQuizzes/Quiz/Model/Quiz.java
@@ -1,43 +1,34 @@
 package com.QualityQuizzes.Quiz.Model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import java.util.List;
 @Entity
 @Table (name = "quiz")
 public class Quiz {
+    // TODO: Figure out what to do with this inner class of questions.
+    // TODO: And how it interact as table in the database.
+    
     //  Inner class of QuizQuestions that will populate a Quiz.
     private static class QuizQuestions {
         // Members ////////////////////////////////////////////////////////////////////////////////////////////////////
-        @Id
-        @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "quiz_generator")
-        private long id;
-       
-        @Column(
-         name             = "quiz_name",
-         nullable         = false,
-         columnDefinition = "TEXT"
-       )
+        
        final private String  questionName;
        
-       @Column(
-         name             = "quiz_number",
-         nullable         = false,
-         columnDefinition = "TEXT"
-       )
        final private  int    questionNumber;
        
-       @Column(
-         name = "quiz_type",
-         nullable = false,
-         columnDefinition = "TEXT"
-       )
        final private  String questionType;
-
+       
        // Methods /////////////////////////////////////////////////////////////////////////////////////////////////////
         QuizQuestions (
             String questionName,
@@ -54,20 +45,42 @@ public class Quiz {
         public int    getQuestionNumber() {return  questionNumber;}
         public String getQuestionType()   {return  questionType;}
     }
-
+    
+    
     // Members ////////////////////////////////////////////////////////////////////////////////////////////////////////
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "quiz_generator")
+    private long id;
+    
+    @ManyToOne (fetch = FetchType.LAZY, optional = false)
+    @JoinColumn (name = "user_id", nullable = false)
+    @OnDelete(action =  OnDeleteAction.CASCADE)
+    @JsonIgnore
+    private ApplicationUser user;
+    @Column(
+      name = "quiz_name",
+      nullable = false,
+      columnDefinition = "TEXT"
+    )
     final private String              quizName;
     final private List<QuizQuestions> quizQuestions;
+    @Column(
+      name = "quiz_question_amount",
+      nullable = false,
+      columnDefinition = "TEXT"
+    )
     final private int                 quizQuestionAmount;
 
     //Methods /////////////////////////////////////////////////////////////////////////////////////////////////////////
     Quiz(
             String              quizName,
             List<QuizQuestions> quizQuestions,
-            int                 quizQuestionAmount
+            int                 quizQuestionAmount,
+            ApplicationUser     user
     ) {
         this.quizName           = quizName;
         this.quizQuestions      = quizQuestions;
         this.quizQuestionAmount = quizQuestionAmount;
+        this.user               = user;
     }
 }
