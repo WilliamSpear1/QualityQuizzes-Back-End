2dc28cc974518aa72be56b759fae31562598ed08
William Spearman <wspearman@protonmail.com>
2022-10-14 14:45:31 -0400

    Created initial repositories for Student and Teacher.

--- src/main/java/com/QualityQuizzes/Quiz/Repository/StudentRepository.java
+++ src/main/java/com/QualityQuizzes/Quiz/Repository/StudentRepository.java
@@ -1,4 +1,12 @@
 package com.QualityQuizzes.Quiz.Repository;
 
-public interface StudentRepository {
+import com.QualityQuizzes.Quiz.Model.Student;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface StudentRepository extends JpaRepository<Student,Long> {
+    // Methods /////////////////////////////////////////////////////////////////////////////////////////////////////
+    List<Student> findByUserName (String username);
+    List<Student> findByStudent  (boolean isStudent);
 }
--- src/main/java/com/QualityQuizzes/Quiz/Repository/TeacherRepository.java
+++ src/main/java/com/QualityQuizzes/Quiz/Repository/TeacherRepository.java
@@ -1,4 +1,10 @@
 package com.QualityQuizzes.Quiz.Repository;
 
-public interface TeacherRepository {
+import com.QualityQuizzes.Quiz.Model.Teacher;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface TeacherRepository extends JpaRepository<Teacher, Long> {
+    List<Teacher> findByTeacher (boolean isTeacher);
 }
