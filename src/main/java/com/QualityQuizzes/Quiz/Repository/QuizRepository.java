a7b61413770d08df1ff20de8f47c10e7da1368c5
William Spearman <wspearman@protonmail.com>
2022-10-29 18:23:16 -0400

    Removed methods from Repositories and leaving them empty for now.

--- src/main/java/com/QualityQuizzes/Quiz/Repository/QuizRepository.java
+++ src/main/java/com/QualityQuizzes/Quiz/Repository/QuizRepository.java
@@ -3,8 +3,5 @@ package com.QualityQuizzes.Quiz.Repository;
 import com.QualityQuizzes.Quiz.Model.Quiz;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
-
 public interface QuizRepository extends JpaRepository<Quiz, Long> {
-    List<Quiz> findByQuizName (String quizName);
 }
--- src/main/java/com/QualityQuizzes/Quiz/Repository/StudentRepository.java
+++ src/main/java/com/QualityQuizzes/Quiz/Repository/StudentRepository.java
@@ -3,10 +3,6 @@ package com.QualityQuizzes.Quiz.Repository;
 import com.QualityQuizzes.Quiz.Model.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
-
-public interface StudentRepository extends JpaRepository<Student,Long> {
+public interface StudentRepository extends JpaRepository<Student, Long> {
     // Methods /////////////////////////////////////////////////////////////////////////////////////////////////////
-    List<Student> findByUserName (String username);
-    List<Student> findByStudent  (boolean isStudent);
 }
