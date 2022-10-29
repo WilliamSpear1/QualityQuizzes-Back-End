dfd796e179a69c23409d0831c5667a8531e3f95e
William Spearman <wspearman@protonmail.com>
2022-10-28 23:21:55 -0400

    Removed Unneeded attributes of the class.

--- src/main/java/com/QualityQuizzes/Quiz/Model/Teacher.java
+++ src/main/java/com/QualityQuizzes/Quiz/Model/Teacher.java
@@ -2,78 +2,44 @@ package com.QualityQuizzes.Quiz.Model;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
 @Table (name = "teacher")
 public class Teacher extends ApplicationUser {
    // Constants ///////////////////////////////////////////////////////////////////////////////////////////////////////
+   @Column
    private static final boolean isTeacher = true;
-
-   // Members /////////////////////////////////////////////////////////////////////////////////////////////////////////
-   @Id
-   @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "teacher_generator")
-   private long   id;
-   @Column(
-    name             = "teacher_user_name",
-    nullable         = false,
-    columnDefinition = "TEXT"
-   )
-   private String teacherUserName;
-   
-   @Column(
-    name = "teacher_email",
-    nullable = false,
-    columnDefinition = "TEXT"
-   )
-   private String teacherEmail;
-   
-   @Column(
-    name = "teacher_first_name",
-    nullable = false,
-    columnDefinition = "TEXT"
-   )
-   private String teacherFirstName;
-   
-   @Column(
-    name = "teacher_last_name",
-    nullable = false,
-    columnDefinition = "TEXT"
-   )
-   private String teacherLastName;
    
    // Methods ////////////////////////////////////////////////////////////////////////////////////////////////////////
    Teacher() {}
    public Teacher(
-     String teacherFirstName,
-     String teacherLastName,
-     String teacherUserName,
-     String teacherEmail,
+     String firstName,
+     String lastName,
+     String userName,
+     String email,
      Long   id
    ) {
         super(
-            teacherFirstName,
-            teacherLastName,
-            teacherUserName,
-            teacherEmail,
+            firstName,
+            lastName,
+            userName,
+            email,
             id
         );
    }
    
    public Teacher(
-     String teacherFirstName,
-     String teacherLastName,
-     String teacherUserName,
-     String teacherEmail
+     String firstName,
+     String lastName,
+     String userName,
+     String email
    ) {
       super(
-        teacherFirstName,
-        teacherLastName,
-        teacherUserName,
-        teacherEmail
+        firstName,
+        lastName,
+        userName,
+        email
       );
    }
    public Boolean getStatus() {return isTeacher;}
