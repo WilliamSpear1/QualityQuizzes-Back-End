f1f906096b4b6f2c955ceb6fbc7747491e9e7118
William Spearman <wspearman@protonmail.com>
2022-10-28 23:10:55 -0400

    Removed class attributes not needed led to shadowing issue, also added column for isTeacher.

--- src/main/java/com/QualityQuizzes/Quiz/Model/Student.java
+++ src/main/java/com/QualityQuizzes/Quiz/Model/Student.java
@@ -2,49 +2,14 @@ package com.QualityQuizzes.Quiz.Model;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
 @Table  (name = "student")
 public class Student extends ApplicationUser {
     // Constants ///////////////////////////////////////////////////////////////////////////////////////////////////////
+    @Column
     private final static boolean isTeacher = false;
-    
-    // Members ////////////////////////////////////////////////////////////////////////////////////////////////////////
-    @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "student_generator")
-    private long   id;
-    
-    @Column(
-      name             = "student_user_name",
-      nullable         = false,
-      columnDefinition = "TEXT"
-    )
-    private String studentUserName;
-    
-    @Column(
-      name             = "student_first_name",
-      nullable         = false,
-      columnDefinition = "TEXT"
-    )
-    private String studentFirstName;
-    
-    @Column(
-      name             = "student_last_name",
-      nullable         = false,
-      columnDefinition = "TEXT"
-    )
-    private String studentLastName;
-    
-    @Column(
-      name             = "student_email",
-      nullable         = false,
-      columnDefinition = "TEXT"
-    )
-    private String studentEmail;
     
     // Methods ////////////////////////////////////////////////////////////////////////////////////////////////////////
     public Student(){}
@@ -62,17 +27,17 @@ public class Student extends ApplicationUser {
         );
     }
     public Student(
-      String studentFirstName,
-      String studentLastName,
-      String studentUserName,
-      String studentEmail,
+      String firstName,
+      String lastName,
+      String userName,
+      String email,
       Long   id
     ) {
         super(
-          studentFirstName,
-          studentLastName,
-          studentUserName,
-          studentEmail,
+          firstName,
+          lastName,
+          userName,
+          email,
           id
         );
     }
