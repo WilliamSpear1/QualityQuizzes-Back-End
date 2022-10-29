caec5a61ca0d76feccc562ca14f4932d70711843
William Spearman <wspearman@protonmail.com>
2022-10-28 22:22:56 -0400

    Changed to abstract Class and added table columns and id to the abstract ApplicationUser class.

--- src/main/java/com/QualityQuizzes/Quiz/Model/ApplicationUser.java
+++ src/main/java/com/QualityQuizzes/Quiz/Model/ApplicationUser.java
@@ -1,15 +1,45 @@
 package com.QualityQuizzes.Quiz.Model;
 
-public class ApplicationUser {
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+
+@MappedSuperclass
+public abstract class ApplicationUser {
     // Members ////////////////////////////////////////////////////////////////////////////////////////////////////////
-    private long id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "user_generator")
+    private long   id;
     
-    private String  email;
-    
-    private String  firstName;
-    private String  lastName;
-    
+    @Column(
+      name             = "user_name",
+      nullable         = false,
+      columnDefinition = "TEXT"
+    )
     private String userName;
+    
+    @Column(
+      name             = "first_name",
+      nullable         = false,
+      columnDefinition = "TEXT"
+    )
+    private String firstName;
+    
+    @Column(
+      name             = "last_name",
+      nullable         = false,
+      columnDefinition = "TEXT"
+    )
+    private String lastName;
+    
+    @Column(
+      name             = "email",
+      nullable         = false,
+      columnDefinition = "TEXT"
+    )
+    private String email;
     
     // Methods /////////////////////////////////////////////////////////////////////////////////////////////////////////
     ApplicationUser() {
