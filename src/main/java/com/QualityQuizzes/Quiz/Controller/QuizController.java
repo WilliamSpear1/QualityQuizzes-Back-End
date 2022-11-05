8be5460412fbbd7faaadda7563fbab6dc8c5c861
William Spearman <wspearman@protonmail.com>
2022-11-05 16:01:25 -0400

    Added Basic CRUD operations to the QuizController.

--- src/main/java/com/QualityQuizzes/Quiz/Controller/QuizController.java
+++ src/main/java/com/QualityQuizzes/Quiz/Controller/QuizController.java
@@ -6,10 +6,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
 
 @CrossOrigin(origins = "http://localhost:8080")
 @RestController
@@ -35,5 +40,31 @@ public class QuizController {
             return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
-   // TODO: Finish implementing the methods for Quiz Controller.
+    
+    @PutMapping("/quiz/{id}")
+    public ResponseEntity<Quiz> updateQuiz(@PathVariable("id") long id, @RequestBody Quiz quiz) {
+        Optional<Quiz> quizData = quizRepository.findById(id);
+        
+        if(quizData.isPresent()) {
+            Quiz _quiz;
+            
+            _quiz = quizData.get();
+            
+            _quiz.setQuizName (quiz.getQuizName());
+            _quiz.addQuizQuestions(quiz.getQuizQuestions());
+            
+            return new ResponseEntity<>(quizRepository.save(_quiz), HttpStatus.OK);
+        }
+        
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+    @DeleteMapping("/quiz/{id}")
+    public ResponseEntity<HttpStatus> deleteQuiz(@PathVariable("id") long id, @RequestBody Quiz quiz) {
+        try {
+            quizRepository.deleteById(id);
+            return new ResponseEntity<>(HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
 }
