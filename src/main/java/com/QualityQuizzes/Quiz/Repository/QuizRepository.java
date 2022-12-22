98dda4339d4a06ceb706f6e2bf977f422de65462
William Spearman <wspearman@protonmail.com>
2022-12-22 16:20:57 -0500

    Deleted Repository and Unit tests.
    Changing the way the database is connected and used therefore deleting previous tests and repositories.

--- src/main/java/com/QualityQuizzes/Quiz/Repository/QuizRepository.java
+++
@@ -1,7 +0,0 @@
-package com.QualityQuizzes.Quiz.Repository;
-
-import com.QualityQuizzes.Quiz.Model.Quiz;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface QuizRepository extends JpaRepository<Quiz, Long> {
-}
--- src/main/java/com/QualityQuizzes/Quiz/Repository/TeacherRepository.java
+++
@@ -1,7 +0,0 @@
-package com.QualityQuizzes.Quiz.Repository;
-
-import com.QualityQuizzes.Quiz.Model.Teacher;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TeacherRepository extends JpaRepository<Teacher, Long> {
-}
