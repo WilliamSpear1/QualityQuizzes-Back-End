45fc0c8d416639f2e1861ceebf43c392a64fdbf1
William Spearman <wspearman@protonmail.com>
2022-12-28 13:51:15 -0500

    Added the data and schema for the teachers database and tables.

--- src/test/resources/data.sql
+++ src/test/resources/data.sql
@@ -2,3 +2,8 @@ insert into students values ('Bret',    'Steadman', 'steadman1', 'steadman1@emai
 insert into students values ('William', 'Louise',   'louise1',   'louise1@email.com',   3L);
 insert into students values ('Henry',   'Cash',     'cash1',     'cash1@email.com',     4L);
 insert into students values ('John',    'Marvin',   'marvin1',   'marvin1@email.com',   5L);
+
+insert into teachers values ('Bret',    'Steadman', 'steadman1', 'steadman1@email.com', 2L);
+insert into teachers values ('William', 'Louise',   'louise1',   'louise1@email.com',   3L);
+insert into teachers values ('Henry',   'Cash',     'cash1',     'cash1@email.com',     4L);
+insert into teachers values ('John',    'Marvin',   'marvin1',   'marvin1@email.com',   5L);
--- src/test/resources/schema.sql
+++ src/test/resources/schema.sql
@@ -6,3 +6,12 @@ create table students (
     id int,
     primary key(id)
 );
+
+create table teachers (
+    firstName varchar(255),
+    lastName varchar(255),
+    userName varchar(255),
+    email varchar(255),
+    id int,
+    primary key(id)
+);
