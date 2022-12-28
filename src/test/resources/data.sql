3be42a3f53152c7956dbd07423f1d74315c8ec9a
William Spearman <wspearman@protonmail.com>
2022-12-28 13:44:33 -0500

    Added Database and table creation along with data population.

---
+++ src/test/resources/data.sql
@@ -0,0 +1,4 @@
+insert into students values ('Bret',    'Steadman', 'steadman1', 'steadman1@email.com', 2L);
+insert into students values ('William', 'Louise',   'louise1',   'louise1@email.com',   3L);
+insert into students values ('Henry',   'Cash',     'cash1',     'cash1@email.com',     4L);
+insert into students values ('John',    'Marvin',   'marvin1',   'marvin1@email.com',   5L);
---
+++ src/test/resources/schema.sql
@@ -0,0 +1,8 @@
+create table students (
+    firstName varchar(255),
+    lastName varchar(255),
+    userName varchar(255),
+    email varchar(255),
+    id int,
+    primary key(id)
+);
