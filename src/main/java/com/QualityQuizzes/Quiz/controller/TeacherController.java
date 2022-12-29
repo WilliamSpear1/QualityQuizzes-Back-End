79f054da99649ab495c281ef5c960b038ef353b3
William Spearman <wspearman@protonmail.com>
2022-12-29 11:24:33 -0500

    Updated the teacher controller, to have use the DAO updateUser method.

--- src/main/java/com/QualityQuizzes/Quiz/controller/TeacherController.java
+++ src/main/java/com/QualityQuizzes/Quiz/controller/TeacherController.java
@@ -15,69 +15,51 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-
 @CrossOrigin(origins = "http://localhost:8080")
 @RestController
 @RequestMapping("/api")
 public class TeacherController {
-    
     // Members ////////////////////////////////////////////////////////////////////////////////////////////////////////
-    private final TeacherDAO teacherDAO;
-    
     private static final Logger logger = LogManager.getLogger();
     
-    public TeacherController (final TeacherDAO teacherDAO) {
-       this.teacherDAO = teacherDAO;
-    }
+    private final TeacherDAO teacherDAO;
+    
+    public TeacherController (final TeacherDAO teacherDAO) { this.teacherDAO = teacherDAO; }
     
     // Methods ////////////////////////////////////////////////////////////////////////////////////////////////////////
     @PostMapping("/teachers")
     public ResponseEntity<Teacher> createTeacher(@RequestBody Teacher teacher) {
         try{
-            final Teacher _teacher = teacherDAO.addUser(
-              new Teacher(
-                teacher.getFirstName(),
-                teacher.getLastName(),
-                teacher.getUserName(),
-                teacher.getEmail()
-              )
-            );
-            logger.info("Created Teacher with the following id" + _teacher.getId());
-            return new ResponseEntity<>(_teacher, HttpStatus.CREATED);
+            teacherDAO.addUser(teacher);
+            logger.info("Created Teacher with the following id" + teacher.getId());
+            return new ResponseEntity<>(teacher, HttpStatus.CREATED);
         }catch (Exception e) {
-            logger.error(e.getMessage());
+            logger.error("Exception was thrown here is the error message: " + e.getMessage());
             return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
     
     @PutMapping("/teachers/{id}")
     public ResponseEntity<Teacher> updateTeacher(@PathVariable("id") long id, @RequestBody Teacher teacher) {
-//        Optional<Teacher> teacherData = teacherRepository.findById(id);
-//
-//        if(teacherData.isPresent()) {
-//            Teacher _teacher;
-//
-//            _teacher = teacherData.get();
-//
-//            _teacher.setFirstName (teacher.getFirstName());
-//            _teacher.setLastName  (teacher.getLastName());
-//            _teacher.setUserName  (teacher.getUserName());
-//            _teacher.setEmailName (teacher.getEmail());
-//
-//            return new ResponseEntity<>(teacherRepository.save(_teacher), HttpStatus.OK);
-//        }
-        
-        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        try{
+            teacherDAO.updateUser(teacher, id);
+            logger.info("Updated Teacher with the following id: " + teacher.getId());
+            return new ResponseEntity<>(teacher, HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
     }
     
     @DeleteMapping("/teachers/{id}")
     public ResponseEntity<HttpStatus> deleteTeacher(@PathVariable("id") long id) {
         try {
             teacherDAO.deleteUser(id);
-            logger.info("Deleted Teacher with the following id" + id);
+            
+            logger.info("Deleted Teacher with the following id: " + id);
+            
             return new ResponseEntity<>(HttpStatus.OK);
         }catch (Exception e) {
-            logger.error(e.getMessage());
+            logger.error("Exception is thrown: " + e);
             return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
