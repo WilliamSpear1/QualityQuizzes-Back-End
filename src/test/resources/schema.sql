38c2115aef4dd583fa3e03a25e35a4a0166dbb32
William Spearman <wspearman@protonmail.com>
2023-01-12 20:42:04 -0500

    Made Table and Column names use Capital letters, remove quizquestion table.

--- src/test/resources/schema.sql
+++ src/test/resources/schema.sql
@@ -1,17 +1,24 @@
-create table students (
-    firstName varchar(255),
-    lastName varchar(255),
-    userName varchar(255),
-    email varchar(255),
-    id int,
-    primary key(id)
+create table STUDENTS (
+    FIRSTNAME varchar(255),
+    LASTNAME varchar(255),
+    USERNAME varchar(255),
+    EMAIL varchar(255),
+    STUDENTID int,
+    primary key(STUDENTID)
 );
 
-create table teachers (
-    firstName varchar(255),
-    lastName varchar(255),
-    userName varchar(255),
-    email varchar(255),
-    id int,
-    primary key(id)
+create table TEACHERS (
+    FIRSTNAME varchar(255),
+    LASTNAME varchar(255),
+    USERNAME varchar(255),
+    EMAIL varchar(255),
+    TEACHERID int,
+    primary key(TEACHERID)
+);
+
+create table QUIZZES (
+    QUIZID int,
+    QUIZSIZE int,
+    QUIZNAME varchar(255),
+    primary key(QUIZID)
 );
