f773af7ac8ef1cc7c3a8c107b811b53225ce39ed
William Spearman <wspearman@protonmail.com>
2023-01-14 12:22:12 -0500

    Added QUIZ table data for testing.

--- src/test/resources/data.sql
+++ src/test/resources/data.sql
@@ -1,9 +1,17 @@
-insert into students values ('Bret',    'Steadman', 'steadman1', 'steadman1@email.com', 2L);
-insert into students values ('William', 'Louise',   'louise1',   'louise1@email.com',   3L);
-insert into students values ('Henry',   'Cash',     'cash1',     'cash1@email.com',     4L);
-insert into students values ('John',    'Marvin',   'marvin1',   'marvin1@email.com',   5L);
+--DATA FOR STUDENTS TABLE
+INSERT INTO STUDENTS VALUES ('Bret',    'Steadman', 'steadman1', 'steadman1@email.com', 2L);
+INSERT INTO STUDENTS VALUES ('William', 'Louise',   'louise1',   'louise1@email.com',   3L);
+INSERT INTO STUDENTS VALUES ('Henry',   'Cash',     'cash1',     'cash1@email.com',     4L);
+INSERT INTO STUDENTS VALUES ('John',    'Marvin',   'marvin1',   'marvin1@email.com',   5L);
 
-insert into teachers values ('Bret',    'Steadman', 'steadman1', 'steadman1@email.com', 2L);
-insert into teachers values ('William', 'Louise',   'louise1',   'louise1@email.com',   3L);
-insert into teachers values ('Henry',   'Cash',     'cash1',     'cash1@email.com',     4L);
-insert into teachers values ('John',    'Marvin',   'marvin1',   'marvin1@email.com',   5L);
+--DATA FOR STUDENTS TABLE
+INSERT INTO TEACHERS VALUES ('Bret',    'Steadman', 'steadman1', 'steadman1@email.com', 2L);
+INSERT INTO TEACHERS VALUES ('William', 'Louise',   'louise1',   'louise1@email.com',   3L);
+INSERT INTO TEACHERS VALUES ('Henry',   'Cash',     'cash1',     'cash1@email.com',     4L);
+INSERT INTO TEACHERS VALUES ('John',    'Marvin',   'marvin1',   'marvin1@email.com',   5L);
+
+--DATA FOR QUIZ TABLE
+INSERT INTO QUIZZES VALUES (2L, 'History Quiz', 10);
+INSERT INTO QUIZZES VALUES (3L, 'Math Quiz',    9);
+INSERT INTO QUIZZES VALUES (4L, 'Science Quiz', 8);
+INSERT INTO QUIZZES VALUES (5L, 'Writing Quiz', 7);
