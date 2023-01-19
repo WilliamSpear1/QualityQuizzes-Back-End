c340610b4c81967376931e51011e52365aa2faa5
William Spearman <wspearman@protonmail.com>
2023-01-18 22:21:22 -0500

    Added the Quiz and QuizQuestion DAO, additionally added the constructor.

--- src/main/java/com/qualityquizzes/quiz/controller/QuizController.java
+++ src/main/java/com/qualityquizzes/quiz/controller/QuizController.java
@@ -1,5 +1,9 @@
 package com.qualityquizzes.quiz.controller;
 
+import com.qualityquizzes.quiz.daoimpl.QuizDAOImpl;
+import com.qualityquizzes.quiz.daoimpl.QuizQuestionDAOImpl;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,5 +12,19 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api")
 public class QuizController {
+    // Members ////////////////////////////////////////////////////////////////////////////////////////////////////////
+    private static final Logger logger = LogManager.getLogger();
+
+    private final QuizQuestionDAOImpl quizQuestionDAO;
+
+    private final QuizDAOImpl quizDAO;
+
+    public QuizController (final QuizQuestionDAOImpl quizQuestionDAO, final QuizDAOImpl quizDAO) {
+        this.quizQuestionDAO = quizQuestionDAO;
+        this.quizDAO         = quizDAO;
+    }
+    
     // Methods ////////////////////////////////////////////////////////////////////////////////////////////////////////
+    // TODO: Implement Controller methods and figure out
+    //  what how to create quizzes and quiz questions.
 }
