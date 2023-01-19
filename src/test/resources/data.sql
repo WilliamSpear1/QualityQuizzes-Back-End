955ac680f9c92c82cc2e70aa36329e60ebf2a472
William Spearman <wspearman@protonmail.com>
2023-01-18 22:09:11 -0500

    Added data for QuizQuestions table and updated to capital letters for schema.

--- src/test/resources/data.sql
+++ src/test/resources/data.sql
@@ -15,3 +15,10 @@ INSERT INTO QUIZZES VALUES (2L, 'History Quiz', 10);
 INSERT INTO QUIZZES VALUES (3L, 'Math Quiz',    9);
 INSERT INTO QUIZZES VALUES (4L, 'Science Quiz', 8);
 INSERT INTO QUIZZES VALUES (5L, 'Writing Quiz', 7);
+
+--DATA FOR QUIZQUESTIONS TABLE
+INSERT INTO QUIZQUESTIONS VALUES (2L, 6L, 'Is Joe Biden President?', 'True',  'False');
+INSERT INTO QUIZQUESTIONS VALUES (3L, 7L, 'Is 4+4 = 2', 'True', 'False');
+INSERT INTO QUIZQUESTIONS VALUES (4L, 8L, 'Is America a nation?', 'True', 'False');
+INSERT INTO QUIZQUESTIONS VALUES (5L, 9L, 'Did John Milton write Paradise Lost?', 'Yes', 'No');
+INSERT INTO QUIZQUESTIONS VALUES (5L, 1L, 'Is John Milton Alive?', 'Yes', 'No');
