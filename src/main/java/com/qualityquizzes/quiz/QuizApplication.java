9f68d8c5e854c705ae3abfcff2c5899473c20a6b
William Spearman <wspearman@protonmail.com>
2023-01-19 18:36:13 -0500

    Added section header.

--- src/main/java/com/qualityquizzes/quiz/QuizApplication.java
+++ src/main/java/com/qualityquizzes/quiz/QuizApplication.java
@@ -5,9 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class QuizApplication {
-
+	// Method ////////////////////////////////////////////////////////////////////////////////////////////////////////
 	public static void main(String[] args) {
 		SpringApplication.run(QuizApplication.class, args);
 	}
-
 }
